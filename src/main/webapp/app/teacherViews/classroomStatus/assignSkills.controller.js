9f1c3e0d0a5a5380682262cf597382882b9f3ad7
mRitchhart <mikeritchhart@earthlink.net>
2016-08-02 21:58:09 -0400

    July Updates v9

--- src/main/webapp/app/teacherViews/classroomStatus/assignSkills.controller.js
+++ src/main/webapp/app/teacherViews/classroomStatus/assignSkills.controller.js
@@ -1,19 +1,126 @@
 angular.module('hopeRanchLearningAcademyApp')
-    .controller('AssignSkillsController', function ($scope, $rootScope, $stateParams, $http, entity, socialSkills, Social_skill, Student_social_skill, skillDataService, Enrolled_SSS) {
+    .controller('AssignSkillsController', function ($scope, $rootScope, $stateParams, $http, entity, socialSkills, Social_skill, Student_Skills, Skill_Data, Student_social_skill, skillDataService, Enrolled_SSS) {
 
         $scope.skills = [];
-        $scope.currentSkills = [];
+        $scope.currentSkills = null;
         $scope.enrolledSkillsArray = [];
         $scope.allSkills = [];
         $scope.allSkillsRetrieved = false;
+        $scope.gatheredAllData = false;
+        $scope.skillIds = null;
+
+
+
+
+        $scope.getSkillData = function(stuId, skillId, skillName, sssId, sssDesc, sssStatus, skillLevel) {
+            Skill_Data.get({stuId: stuId, skillId: skillId}, function(result){
+
+                console.log("GETTING SKILL DATA FOR SKILL ID: " + skillId);
+                console.log(result);
+                if (result.points == null && result.skillName == null){
+                    var valueAndName = {};
+                    valueAndName.skillId = skillId;
+                    valueAndName.name = skillName;
+                    valueAndName.points = 0;
+                    valueAndName.sssId = sssId;
+                    valueAndName.sssDesc = sssDesc;
+                    valueAndName.sssStatus = sssStatus;
+                    valueAndName.level = skillLevel;
+                } else {
+                    var valueAndName = {"points": result.points, "name": result.skillName, "skillId": result.skillId, "sssId": sssId, "sssDesc": sssDesc, "sssStatus": sssStatus, "level": skillLevel};
+                }
+                $scope.skillInfo.skillPointsAndNames.push(valueAndName);
+                $scope.numSkillDataRetrieved++;
+            });
+        };
+
+        $scope.getSkillIds = function(stuId) {
+
+            // get an array of all Social Skill IDs associated with the student
+            console.log("student id: " + stuId);
+            Student_Skills.get({stuId: stuId}, function(result){
+                // if (result = []){console.log("null data returned"); return;}
+                console.log(result);
+                var skillIds = [];
+                var skillNames = [];
+                var sssIds = [];
+                var sssDescriptions = [];
+                var sssStatuses = [];
+                var skillLevels = [];
+                $scope.skillIdsAndNames = result;
+                for (var i=0; i<result.skills.length; i++) {
+                    var currentSkillID = result.skills[i][0];
+                    var currentSkillName = result.skills[i][1];
+                    var currentSssId = result.skills[i][2];
+                    var currentSssDesc = result.skills[i][3];
+                    var currentSssStatus = result.skills[i][4];
+                    var currentSkillLevel = result.skills[i][5];
+                    skillIds.push(currentSkillID);
+                    skillNames.push(currentSkillName);
+                    sssIds.push(currentSssId);
+                    sssDescriptions.push(currentSssDesc);
+                    sssStatuses.push(currentSssStatus);
+                    skillLevels.push(currentSkillLevel);
+                    // get the points Sum and Skill Name for each skill associated with student
+                }
+                console.log(skillIds);
+                $scope.skillIds = skillIds;
+                $scope.skillNames = skillNames;
+
+                $scope.skillInfo = {};
+                $scope.skillInfo.skillPointsAndNames = [];
+                $scope.numSkillDataRetrieved = 0;
+
+                if (skillIds.length < 1) {
+                    $scope.skillInfo = {};
+                    $scope.skillInfo.skillPointsAndNames = [];
+                    console.log("no skill Data to retrieve");
+                    $scope.gatheredAllData = true;
+
+                }
+                else {
+                    //console.log("skillIds array length = " + skillIds.length);
+                    for (var j=0;j<skillIds.length;j++){
+                        $scope.getSkillData(stuId, skillIds[j], skillNames[j], sssIds[j], sssDescriptions[j], sssStatuses[j], skillLevels[j]);
+                        //console.log("j = " + j);
+                        if (j == skillIds.length - 1) {
+                            //console.log("j = " + j);
+                            $scope.gatheredAllData = true;
+                            console.log("gathered all skill data");
+                        }
+
+                    }
+                }
+
+            });
+        };
+
+        $scope.$watch("numSkillDataRetrieved", function(newValue, oldValue) {
+            console.log("Number of SkillData retrieved = " + newValue);
+            if ($scope.skillIds != null) {
+                if (newValue == $scope.skillIds.length) {
+                    console.log(newValue + " Skill Data Objects Retrieved");
+                    console.log("all skill Data retrieved and seen by watcher");
+                    $scope.oldSkillPointsAndNames = null;
+                    $scope.currentSkills = $scope.skillInfo.skillPointsAndNames;
+                    console.log($scope.currentSkills);
+                }
+            }
+        });
+
+
+
+
+
+
 
         $scope.loadNeededResources = function() {
+            $scope.student = entity;
             Social_skill.query(function(result) {
                $scope.social_Skills = result;
                //sortAvailable($scope.social_Skills);
+                $scope.getSkillIds($scope.student.id);
             });
-            $scope.student = entity;
-            $scope.currentSkills = skillDataService.get();
         };
 
         $scope.loadNeededResources();
@@ -21,7 +128,8 @@ angular.module('hopeRanchLearningAcademyApp')
         var sortAvailable = function() {
             var enrolledSkillsArray = [];
             var IDsToRemove = [];
-            var duplicateSkills = []
+            var duplicateSkills = [];
+            console.log($scope.currentSkills);
             for (var i=0; i<$scope.allSkills.length;i++) {
                 for (var k=0; k<$scope.currentSkills.length;k++) {
                     if ($scope.allSkills[i].id == $scope.currentSkills[k].skillId) {
@@ -50,6 +158,7 @@ angular.module('hopeRanchLearningAcademyApp')
         };
 
         $scope.$watchGroup(["currentSkills", "social_Skills"], function(newValue, oldValue) {
+            console.log(newValue[0]);
             if (newValue[0] != null && newValue[0] != undefined && newValue[1] != [] && newValue[1] != undefined) {
                 console.log("all Data retrieved");
 
